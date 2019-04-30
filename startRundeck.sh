f24cc7d95cf2c53d30301834484559dd55d548a1
Dr. Stefan Reisner <reisners@users.noreply.github.com>
2019-04-30 21:33:36 +0200

    Update startRundeck.sh

--- startRundeck.sh
+++ startRundeck.sh
@@ -10,7 +10,7 @@ export PATH=$PATH:${BASE_PATH}/.java/bin
 
 echo "-----> Starting Rundeck"
 
-ADDITIONAL_ARGS="-Dserver.http.port=${PORT}"
+ADDITIONAL_ARGS="-Dserver.http.port=${PORT} -Xmx1g"
 
 ADDITIONAL_ARGS="${ADDITIONAL_ARGS} -Drundeck.log4j.config.file=$RDECK_BASE/server/config/log4j.properties -Drundeck.jaaslogin=true -Dloginmodule.conf.name=jaas-login.conf -Dloginmodule.name=RDpropertyfilelogin"
 
